c9ada4e7c62b0407f142e3d6b9632ffffa79d396
DESKTOP-RR1G80E\Maxwell <maxsuchin@gmail.com>
2019-12-05 14:55:30 -0500

    added enemy anim

--- project katana/Assets/Enemy.cs
+++ project katana/Assets/Enemy.cs
@@ -12,12 +12,14 @@ public class Enemy : MonoBehaviour
 
     public GameObject projectile;
     public Transform player;
+    private Animator anim;
+    private bool _isrunning;
 
     // Use this for initialization
     void Start()
     {
-        
-            GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+        anim = GetComponent<Animator>();
+        GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
             if (playerObject != null)
             {
                 player = playerObject.transform;
@@ -32,14 +34,17 @@ public class Enemy : MonoBehaviour
         if (Vector2.Distance(transform.position, player.position) > stoppingDistance)
         {
             transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+            anim.SetBool("isRunning", true);
         }
         else if (Vector2.Distance(transform.position, player.position) < stoppingDistance && Vector2.Distance(transform.position, player.position) > retreatDistance)
         {
             transform.position = this.transform.position;
+            anim.SetBool("isRunning", false);
         }
         else if (Vector2.Distance(transform.position, player.position) < retreatDistance)
         {
             transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+            anim.SetBool("isRunning", true);
         }
         if( timeBtwShots <= 0)
         {
